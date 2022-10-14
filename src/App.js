3e3241e0c18eab31bcbb92c62cb7ead0488c8ddd
martin-sainos-demian <martin.sainos.demian@gmail.com>
2022-10-14 14:14:07 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import Login from './comps/Login';
+import TitlePass from './comps/TitlePass';
+import NewPass from './comps/NewPass';
+import GetPass from './comps/GetPass';
+import Encoder from './comps/Encoder';
+import Register from './comps/Register';
+
+
+function App() {
+  return (
+    <div className="App">
+      <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/crypto-js/3.1.2/rollups/aes.js"></script>
+      <TitlePass></TitlePass>
+      <Login id="login"></Login>
+      <NewPass></NewPass>
+      <GetPass></GetPass>
+      <Encoder></Encoder>
+      <Register></Register>
+        <p id="logId" hidden></p>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/comps/NewPass.jsx
@@ -0,0 +1,40 @@
+import React, { Component, startTransition } from 'react'
+import {Button} from 'react-native'
+import RandomPass from './RandomPass';
+import $ from 'jquery'
+import {en,de} from "./sec"
+
+export default class NewPass extends Component {
+
+
+
+    render() {
+      return (
+        <div className="NewPass">
+          <RandomPass></RandomPass>
+          <input type="text" id="newPassName" placeholder='name'></input> 
+          <Button title="save" onPress={savePass}></Button>
+        </div>
+      )
+    }
+  }
+  function savePass(){
+    var usu_id=$("#logId").text()
+    var pass=$("#genPass").val()
+    var name=$("#newPassName").val()
+    if(pass!=""&&usu_id!=""){
+      usu_id=usu_id.substring(1,usu_id.length)
+      usu_id=parseInt(usu_id)
+      console.log(pass+" "+name)
+      pass=en(pass)
+      name=en(name)
+      fetch("https://securitypassword.cyclic.app/savePass/?usu_id="+usu_id+"&pass="+pass+"&name="+name, {method : 'GET',})
+      .then(function(response) {
+        return response.json(); })
+        .then(function(json) {
+          if(json.data!=""){
+            console.log(de(json.data))
+          }
+      })
+    }
+  }
